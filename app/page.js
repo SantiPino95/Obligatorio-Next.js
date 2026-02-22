27adfbea96cc94cf42a0039b636224ce3aabd8b7
santiagopino393 <santipin2009@hotmail.com>
2026-02-22 20:44:49 -0300

    Primer comentario, creacion de extructura del proyecto

---
+++ app/page.js
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <section>
+      <h1>¡Bienvenido a la Home!</h1>
+      <p>Este es el contenido principal de mi página de inicio.</p>
+    </section>
+  );
+}
